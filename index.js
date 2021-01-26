44efe8f9ca72a9550c3f6334a22a8bbc0cae07ef
bengroves <bengroves15@gmail.com>
2021-01-26 15:26:16 -0500

    commit tree 6b28372fec782102e0e1da6fc1eaca91a96bb29d

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+
+
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str) {
+  let con = parseInt(str,10);
+  if (con < 12 ){
+    return 'Good Morning' 
+    }
+    else if (con >= 12 && con <= 17)  {
+      return 'Good Afternoon'
+    }
+    else {
+      return 'Good Evening'
+    }
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage (str) {
+let newHeader = document.getElementById('greeting');
+newHeader.innerText = `${str}`;
+}
